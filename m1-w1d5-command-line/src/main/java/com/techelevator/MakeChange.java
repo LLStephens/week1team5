a58073428d6966148ea826974bc4440b295b14e2
Laura Stephens <laura@neutralspace.org>
2017-01-21 12:15:48 -0500

    Week 1 group assignments completed

--- m1-w1d5-command-line/src/main/java/com/techelevator/MakeChange.java
+++ m1-w1d5-command-line/src/main/java/com/techelevator/MakeChange.java
@@ -1,7 +1,10 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 /*
- Write a command line program which prompts the user for the total bill, and the amount tendered. It should then display the change required.
+ Write a command line program which prompts the user for the total bill, and the amount tendered. 
+ It should then display the change required.
  
  $ java MakeChange
  Please enter the amount of the bill: 23.65
@@ -11,7 +14,24 @@ package com.techelevator;
 public class MakeChange {
 
 	public static void main(String[] args) {
-
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Please enter the amount of the bill: ");
+		double bill = input.nextDouble();
+		input.nextLine();
+		
+		System.out.println("Please enter the amount tendered: ");
+		double amount = input.nextDouble();
+		input.nextLine();
+		
+		if(amount==bill) {
+			System.out.println("No change");
+		} else {
+			double change = amount-bill;
+			System.out.println("The change required is $" + change);
+		}
+		
+		
 	}
 
 }
--- m1-w1d5-command-line/src/main/java/com/techelevator/MartianWeight.java
+++ m1-w1d5-command-line/src/main/java/com/techelevator/MartianWeight.java
@@ -1,5 +1,7 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 /*
  In case you've ever pondered how much you weight on Mars, here's the calculation:
  	Wm = We * 0.378
@@ -19,7 +21,20 @@ Enter a series of Earth weights (space-separated): 98 235 185
 public class MartianWeight {
 
 	public static void main(String[] args) {
-
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter a series of Earth weights (space-separated):");
+		String weights = input.nextLine();
+		String[] weightStrings = weights.split(" ");
+		
+		
+		for(int i = 0; i<weightStrings.length; i++) {
+			Float weight = Float.parseFloat(weightStrings[i]);
+			double marsValue = 0.378;
+			System.out.println(weightStrings[i] + " lbs. on Earth, is " + (weight * marsValue)  + " lbs. on Mars");
+			
+			
+		}
+		
 	}
 
 }
